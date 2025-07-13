8562f11f8697ef78cfd1e189bb740715caecf725
Gustavo Ewald <ewaldgustavo@gmail.com>
2025-07-13 19:54:29 -0300

    Constinuacao dos testes

--- order/tests/test_order_serializer.py
+++ order/tests/test_order_serializer.py
@@ -1,9 +1,10 @@
 import pytest
 
 from django.contrib.auth.models import User
-from order.factories import OrderFactory
+from order.factories import OrderFactory, UserFactory
 from order.serializers.order_serializer import OrderSerializer
 from product.factories import CategoryFactory, ProductFactory
+from product.models.category import Category
 
 @pytest.mark.django_db
 def test_order_serializer_output():
@@ -18,5 +19,29 @@ def test_order_serializer_output():
 
     assert data['total'] == sum([product.price for product in product_list])
     assert len(data['product']) == len(product_list)
-    assert data['product'][0]['price'] == product_1.price
-    assert data['product'][1]['price'] == product_2.price
+    #assert data['product'][0]['price'] == product_1.price
+    #assert data['product'][1]['price'] == product_2.price
+    assert data['product'][0] == product_1.title
+    assert data['product'][1] == product_2.title
+
+@pytest.mark.django_db
+def test_order_deserializer_output():
+    user = UserFactory()
+    category1 = CategoryFactory()
+    category2 = CategoryFactory()
+
+    product1 = ProductFactory(title="first product", price=50, category=[category1])
+    product2 = ProductFactory(title="second product", price=100, category=[category1, category2])
+
+    data = {
+        "product": [
+            product1.title,
+            product2.title
+        ],
+        "user": user.id
+    }
+
+    serializer = OrderSerializer(data=data)
+    assert serializer.is_valid(), serializer.errors
+    order = serializer.save()
+    assert order is not None
