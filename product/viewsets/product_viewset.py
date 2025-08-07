b185543a53dfc487f438307286ffe00997d0aee6
Gustavo Ewald <ewaldgustavo@gmail.com>
2025-08-07 00:22:49 -0300

    Autenticacao da viewset de order

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -3,12 +3,12 @@ from rest_framework import viewsets
 from product.models import Product
 from product.serializers import ProductSerializer
 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+##from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+##from rest_framework.permissions import IsAuthenticated
 
 class ProductViewSet(viewsets.ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    ##authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    ##permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
     
     def get_queryset(self):
